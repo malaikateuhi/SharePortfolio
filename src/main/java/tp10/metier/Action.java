0373a701ad6549da2279f97084d360200120edb6
malaikateuhi <malaikateuhi@gmail.com>
2021-03-22 02:44:34 +0100

    Update Action.java

--- src/main/java/tp10/metier/Action.java
+++ src/main/java/tp10/metier/Action.java
@@ -10,38 +10,61 @@ package tp10.metier;
  */
 public abstract class Action {
 
-    private String nom;
+    /**
+     * Name of the action.
+     */
+    private final String nom;
 
     /**
-     * Get the value of nom
+     * Get the value of nom.
      *
      * @return the value of nom
      */
-    public String getNom() {
+    public final String getNom() {
         return nom;
     }
 
-    public Action(String nom) {
+    /**
+     * Build an object of class Action from a name.
+     *
+     * @param nom name of the action
+     */
+    protected Action(final String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Get the value of an action for a given day.
+     *
+     * @param j the given day
+     * @return the value of the action
+     */
     public abstract float valeur(Jour j);
 
+    /**
+     *
+     * @param obj
+     * @return boolean 
+     */
     @Override
-    public boolean equals(Object obj) {
-        Action actionAComparer;
-        
-        actionAComparer = (Action)obj;
-       return (this.getNom().compareToIgnoreCase(actionAComparer.getNom()) == 0);
+    public final boolean equals(final Object obj) {
+        Action actionAComparer = null;
+
+        if (obj.getClass() != Action.class) {
+            Exception ex = new Exception("Object is not an instance of class Action");
+        } else {
+            actionAComparer = (Action) obj;
+        }
+        return (this.getNom().compareToIgnoreCase(actionAComparer.getNom()) == 0);
     }
 
     @Override
-    public int hashCode() {
+    public final int hashCode() {
         return this.getNom().toUpperCase().hashCode();
     }
-    
+
     @Override
     public String toString() {
         return this.getNom();
     }
-} 
+}
